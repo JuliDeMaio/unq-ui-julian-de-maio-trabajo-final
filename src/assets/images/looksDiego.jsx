fed9844c4a002f613333a14e8abbb851afb02109
JuliDeMaio <julidema82@gmail.com>
2024-12-03 09:09:22 -0300

    se realizó archivo de imagenes + estilo y navegación a homeScreen

---
+++ src/assets/images/looksDiego.jsx
@@ -0,0 +1,23 @@
+import argentina86 from './argentina86.png'
+import argentina94 from './argentina94.png'
+import argentinadt from './argentinadt.png'
+import fifa from './fifa.png'
+import napoli from './napoli.png'
+import palco from './palco.png'
+import platinado from './platinado.png'
+import vincha from './vincha.png'
+
+const looksDiego =
+    [
+        {name:'argentina86', imageSrc: argentina86},
+        {name:'argentina94', imageSrc: argentina94},
+        {name:'argentinadt', imageSrc: argentinadt},
+        {name:'fifa', imageSrc: fifa},
+        {name:'napoli', imageSrc: napoli},
+        {name:'palco', imageSrc: palco},
+        {name:'platinado', imageSrc: platinado},
+        {name:'vincha', imageSrc: vincha},
+
+    ]
+
+export default looksDiego
--- src/pages/HomeScreen.jsx
+++ src/pages/HomeScreen.jsx
@@ -1,15 +1,22 @@
 import React from 'react'
 import { useNavigate  } from 'react-router-dom'
+import '../styles/homescreen.css'
 
 const HomeScreen = () => {
     const navigate = useNavigate();
-
+  
+    const handlePlayClick = () => {
+      navigate('/play');
+    };
+  
     return (
-        <div>
-            <h1>MEMO TEST</h1>
-            <button onClick={() => {navigate('/play')}}>Arranquemos!</button>
-        </div>
-      );
-}
-
-export default HomeScreen
+      <div className="home-screen">
+        <h1 className="title">¡Bienvenido al Memotest!</h1>
+        <button className="play-button" onClick={handlePlayClick}>
+          Arranquemos!
+        </button>
+      </div>
+    )
+  }
+  
+  export default HomeScreen;
