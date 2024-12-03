19f2a977c38b47c4a581afe73b372a79e4fc38c5
JuliDeMaio <julidema82@gmail.com>
2024-12-03 17:43:31 -0300

    se agregan más imágenes para próxima ampliación de tablero

--- src/assets/images/looksDiego.jsx
+++ src/assets/images/looksDiego.jsx
@@ -1,4 +1,6 @@
+import argentina82 from './argentina82.png'
 import argentina86 from './argentina86.png'
+import argentina90 from './argentina90.png'
 import argentina94 from './argentina94.png'
 import argentinadt from './argentinadt.png'
 import fifa from './fifa.png'
@@ -6,10 +8,27 @@ import napoli from './napoli.png'
 import palco from './palco.png'
 import platinado from './platinado.png'
 import vincha from './vincha.png'
+import bocajuniors from './bocajuniors.png'
+import bocajuniorsvuelta from './bocajuniorsvuelta.png'
+import argentinahincha from './argentinahincha.png'
+import cebollitas from './cebollitas.png'
+import argjuniors from './argjuniors.png'
+import barcelona from './barcelona.png'
+import arabia from './arabia.png'
+import conductor from './conductor.png'
+import cuba from './cuba.png'
+import doradossinaloa from './doradossinaloa.png'
+import newells from './newells.png'
+import racing from './racing.png'
+import sub20 from './sub20.png'
+import sobrepeso from './sobrepeso.png'
+import sevilla from './sevilla.png'
 
 const looksDiego =
     [
+        {name:'argentina82', imageSrc: argentina82},
         {name:'argentina86', imageSrc: argentina86},
+        {name:'argentina90', imageSrc: argentina90},
         {name:'argentina94', imageSrc: argentina94},
         {name:'argentinadt', imageSrc: argentinadt},
         {name:'fifa', imageSrc: fifa},
@@ -17,7 +36,21 @@ const looksDiego =
         {name:'palco', imageSrc: palco},
         {name:'platinado', imageSrc: platinado},
         {name:'vincha', imageSrc: vincha},
-
+        {name:'bocajuniors', imageSrc: bocajuniors},
+        {name:'bocajuniorsvuelta', imageSrc: bocajuniorsvuelta},
+        {name:'argentinahincha', imageSrc: argentinahincha},
+        {name:'cebollitas', imageSrc: cebollitas},
+        {name:'argjuniors', imageSrc: argjuniors},
+        {name:'barcelona', imageSrc: barcelona},
+        {name:'arabia', imageSrc: arabia},
+        {name:'conductor', imageSrc: conductor},
+        {name:'cuba', imageSrc: cuba},
+        {name:'doradossinaloa', imageSrc: doradossinaloa},
+        {name:'newells', imageSrc: newells},
+        {name:'racing', imageSrc: racing},
+        {name:'sub20', imageSrc: sub20},
+        {name:'sobrepeso', imageSrc: sobrepeso},
+        {name:'sevilla', imageSrc: sevilla},
     ]
 
 export default looksDiego
